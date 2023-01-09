0ef7b5b2e709114997b5d28287d926f27fc4eee5
José PInheiro <josepsousa2012@gmail.com>
2023-01-09 11:51:02 -0300

    feat - possible moves

--- chess_system/src/chess/ChessMatch.java
+++ chess_system/src/chess/ChessMatch.java
@@ -35,6 +35,8 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPeace(position)) {
 			throw new ChessException("There is no peace on source position");
+		} if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
